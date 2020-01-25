8090d7f08f9741c619ede834c37e2b2e671f3072
Vladimir Hanin <vladimir.hanin@outlook.com>
2020-01-25 22:09:21 +0000

    lines appear from the mouse to one corner of the walls

--- RayTracing.py
+++ RayTracing.py
@@ -4,7 +4,8 @@ This program tries to simulate the lightrays that come from a torch in a room.
 
 import tkinter
 
-
+# ! this list collects all the rectangles that are created
+listOfRect = []
 class Rectangle(object):
     
     # * this initiates the variables of the rectangle, it gives each vertex a value
@@ -26,6 +27,23 @@ class Rectangle(object):
         #creates the rectangle
         c.create_rectangle(x1, y1, x2, y2, outline="grey", fill="grey")
 
+        #adds it to the list
+        listOfRect.append(self)
+
+# ! this list collects all the lines that are created
+listOfLines = []
+class Line(object):
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.x2 = x2
+
+        self.x2 = x2
+        self.y2 = y2
+
+        self.shape = c.create_line(x1, y1, x2, y2, fill="white")
+
+        listOfLines.append(self)
+
 
 # * creation of the window
 root = tkinter.Tk()
@@ -36,9 +54,27 @@ c.configure(bg="black")
 c.pack()
 
 #creates a grey rectangle
-x = Rectangle(100, 75, 500, 125)
+rectTOP = Rectangle(100, 75, 500, 125)
+rectLEFT = Rectangle(100, 250, 150, 500)
+rectRIGHT = Rectangle(450, 250, 500, 500)
 
 
+# * this function gets the coordinates of the mouse and sends those coordinates to the function placeLines
+def motion(event):
+    placeLines(event.x, event.y)
+root.bind('<Motion>', motion)
+
+
+def placeLines(mouseX, mouseY):
+
+
+    for line in listOfLines:
+        c.delete(line.shape)
+        listOfLines.remove(line)
+
+    
+    for rectangle in listOfRect:
+        Line(mouseX, mouseY, rectangle.topLx, rectangle.topLy)
 
 
 # * start program
